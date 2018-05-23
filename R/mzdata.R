bbfe9519ce48cfc128db53d5611879ba421f7cf6
Benjamin R. Gordon <brgordon17@gmail.com>
2018-05-23 12:20:59 +1000

    Initial commit. Data documentation.

---
+++ R/mzdata.R
@@ -0,0 +1,17 @@
+#' Preprocessed LCMS data
+#'
+#' A dataset of positive ESI-LCMS mz variables for coral samples subjected to
+#' experimental ocean warming and acidification at Heron Island in Feb 2011.
+#'
+#' @format A tibble with 101 rows and 1647 variables:
+#' \describe{
+#'   \item{sample_ids}{ID's for each sample}
+#'   \item{class}{Treatment group. One of, control, eT (eleveated temperature),
+#'   eCO2 (elevated CO2), eCO2eT (elevated CO2 and temperature), PBQC}
+#'   \item{day}{Days of exposure to treatment}
+#'   \item{tank}{Tank membership. Either L (left) or R (right)}
+#'   \item{rep}{Sample replicate number}
+#'   \item{class_day}{interaction variable between class and day variables}
+#' }
+#' @source Benjamin R. Gordon
+"mzdata"
---
+++ R/nmrdata.R
@@ -0,0 +1,18 @@
+#' Preprocessed NMR data
+#'
+#' A dataset of 1H-NMR variables (0.02 ppm wide bins) for coral samples
+#' subjected to experimental ocean warming and acidification at Heron Island in
+#' Feb 2011.
+#'
+#' @format A tibble with 93 rows and 505 variables:
+#' \describe{
+#'   \item{sample_ids}{ID's for each sample}
+#'   \item{class}{Treatment group. One of, control, eT (eleveated temperature),
+#'   eCO2 (elevated CO2), eCO2eT (elevated CO2 and temperature), PBQC}
+#'   \item{day}{Days of exposure to treatment}
+#'   \item{tank}{Tank membership. Either L (left) or R (right)}
+#'   \item{rep}{Sample replicate number}
+#'   \item{class_day}{interaction variable between class and day variables}
+#' }
+#' @source Benjamin R. Gordon
+"nmrdata"
