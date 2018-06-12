10479795902b1273a102db860636e9d36ebc3b34
Benjamin R. Gordon <brgordon17@gmail.com>
2018-06-12 14:36:04 +1000

    updated function items; added code to quiet the NOTES produced by R CMD CHECK.

--- R/gordon01.R
+++ R/gordon01.R
@@ -7,8 +7,24 @@
 #'
 #' @section Functions:
 #' \itemize{
-#' \item mzdata_raw
-#' \item mzdata
+#' \item create_litmz()
+#' \item create_mzdata()
+#' \item create_nmrdata()
+#' \item figure_pca()
+#' \item figure_tuning()
+#' \item mzdata_raw()
+#' \item mzpca()
+#' \item mzpls()
+#' \item mzrf()
+#' \item nmrpca()
+#' \item nmrpls()
+#' \item nmrrf()
+#' \item plot_base()
+#' \item qual_colours()
+#' \item sd_colours()
+#' \item seq_colours()
+#' \item table_cv_performance()
+#' \item theme_brg_grid()
 #'}
 #'
 #' @author Benjamin R. Gordon
@@ -16,3 +32,29 @@
 #' @docType package
 #' @name gordon01
 NULL
+
+# Quiets R CMD CHECK notes where there is no visible binding for some global
+# variables
+if(getRversion() >= "2.15.1") {
+  utils::globalVariables(c("Accuracy",
+                           "Endnote Reference",
+                           "Reported m/z",
+                           "Reported ion",
+                           "Monoisotopic Mass (Da)",
+                           "Molecular Formula",
+                           "Reported Compound",
+                           "mtry",
+                           "mzdata",
+                           "scores",
+                           "PC1",
+                           "PC2",
+                           "x_lab",
+                           "y_lab",
+                           "Genus",
+                           "Reported compound name",
+                           "ncomp",
+                           "nmrdata",
+                           "ref",
+                           "save.mzpls"))
+
+}
