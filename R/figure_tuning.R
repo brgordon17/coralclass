2ed40c8a9a9d2c2ffe945b271de0413d1e427ae7
Benjamin R. Gordon <brgordon17@gmail.com>
2019-07-25 21:44:45 +1000

    moved plotting from pls model functions to here

--- R/figure_tuning.R
+++ R/figure_tuning.R
@@ -29,11 +29,144 @@ figure_tuning <- function(view.plot = TRUE,
                           save.plot = FALSE,
                           plot.name = "tuning_figure") {
 
-  # Load gg objects ------------------------------------------------------------
-  mzpls_plot <- readRDS("./inst/extdata/mzpls_tune_ggobject.rds")
-  mzrf_plot <- readRDS("./inst/extdata/mzrf_tune_ggobject.rds")
-  nmrpls_plot <- readRDS("./inst/extdata/nmrpls_tune_ggobject.rds")
-  nmrrf_plot <- readRDS("./inst/extdata/nmrrf_tune_ggobject.rds")
+  # Load models ----------------------------------------------------------------
+  mzpls <- readRDS("./inst/extdata/mzpls_model.rds")
+  mzrf <- readRDS("./inst/extdata/mzrf_model.rds")
+  nmrpls <- readRDS("./inst/extdata/nmrpls_model.rds")
+  nmrrf <- readRDS("./inst/extdata/nmrrf_model.rds")
+
+  # Plotting -------------------------------------------------------------------
+  custom_colours <- phdhelpr::warm_colours[4] # red
+
+  mzpls_plot <- ggplot(mzpls$results,
+                       aes(x = ncomp, y = Accuracy)) +
+    geom_line(colour = custom_colours,
+              size = 1) +
+    geom_point(aes(x = ncomp[ncomp == 16],
+                   y = Accuracy[ncomp == 16]),
+               colour = custom_colours,
+               size = 3) +
+    annotate("text",
+             x = 16,
+             y = 0.8428571 + 0.15,
+             label = "LCMS PLS-DA") +
+    annotate("text",
+             x = 16,
+             y = 0.8428571 + 0.08,
+             label = "Best Tune (16, 0.84)") +
+    scale_x_continuous(limits = c(1, 50),
+                       expand = waiver(),
+                       name = "Number of Components") +
+    scale_y_continuous(limits = c(0, 1),
+                       expand = waiver()) +
+    theme(panel.background = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(colour = "grey90",
+                                            size = 0.6),
+          axis.title = element_text(size = 14),
+          axis.text = element_text(colour = "grey65"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(colour = "grey90",
+                                      size = 0.6),
+          axis.line.x = element_blank())
+
+  nmrpls_plot <- ggplot(nmrpls$results,
+                       aes(x = ncomp, y = Accuracy)) +
+    geom_line(colour = custom_colours,
+              size = 1) +
+    geom_point(aes(x = ncomp[ncomp == 21],
+                   y = Accuracy[ncomp == 21]),
+               colour = custom_colours,
+               size = 3) +
+    annotate("text",
+             x = 21,
+             y = 0.7708333 + 0.15,
+             label = "NMR PLS-DA") +
+    annotate("text",
+             x = 21,
+             y = 0.7708333 + 0.08,
+             label = "Best Tune (21, 0.77)") +
+    scale_x_continuous(limits = c(1, 50),
+                       expand = waiver(),
+                       name = "Number of Components") +
+    scale_y_continuous(limits = c(0, 1),
+                       expand = waiver()) +
+    theme(panel.background = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(colour = "grey90",
+                                            size = 0.6),
+          axis.title = element_text(size = 14),
+          axis.text = element_text(colour = "grey65"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(colour = "grey90",
+                                      size = 0.6),
+          axis.line.x = element_blank())
+
+  mzrf_plot <- ggplot(mzrf$results,
+                      aes(x = mtry,
+                          y = Accuracy)) +
+    geom_line(colour = custom_colours,
+              size = 1) +
+    geom_point(aes(x = mtry[mtry == 350],
+                   y = Accuracy[mtry == 350]),
+               colour = custom_colours,
+               size = 3) +
+    annotate("text",
+             x = 350,
+             y = 0.8428571 + 0.15,
+             label = "LCMS RF") +
+    annotate("text",
+             x = 350,
+             y = 0.8426587 + 0.08,
+             label = "Best Tune (350, 0.84)") +
+    scale_x_continuous(limits = c(10, 550),
+                       expand = c(0, 0),
+                       name = "mtry") +
+    scale_y_continuous(limits = c(0, 1),
+                       expand = waiver()) +
+    theme(panel.background = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(colour = "grey90",
+                                            size = 0.6),
+          axis.title = element_text(size = 14),
+          axis.text = element_text(colour = "grey65"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(colour = "grey90",
+                                      size = 0.6),
+          axis.line.x = element_blank())
+
+  nmrrf_plot <- ggplot(nmrrf$results,
+                      aes(x = mtry,
+                          y = Accuracy)) +
+    geom_line(colour = custom_colours,
+              size = 1) +
+    geom_point(aes(x = mtry[mtry == 25],
+                   y = Accuracy[mtry == 25]),
+               colour = custom_colours,
+               size = 3) +
+    annotate("text",
+             x = 25 + 75,
+             y = 0.6125000 + 0.15,
+             label = "NMR RF") +
+    annotate("text",
+             x = 25 + 75,
+             y = 0.6125000 + 0.08,
+             label = "Best Tune (25, 0.61)") +
+    scale_x_continuous(limits = c(10, 550),
+                       expand = c(0, 0),
+                       name = "mtry") +
+    scale_y_continuous(limits = c(0, 1),
+                       expand = waiver()) +
+    theme(panel.background = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(colour = "grey90",
+                                            size = 0.6),
+          axis.title = element_text(size = 14),
+          axis.text = element_text(colour = "grey65"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(colour = "grey90",
+                                      size = 0.6),
+          axis.line.x = element_blank())
 
   # Set corner labels ----------------------------------------------------------
   mzpls_plot_a <-
